c7839000aaab7a1de0f3d7b9d09ab379026d576e
cornelioov <80319322+cornelioov@users.noreply.github.com>
2023-03-16 10:34:18 +0100

    Añadida P2 (hasta 10/3)

---
+++ Mis prácticas/binary_tree/build/CMakeFiles/test_btree_node.dir/test_btree_node.cpp.o.d
@@ -0,0 +1,186 @@
+CMakeFiles/test_btree_node.dir/test_btree_node.cpp.o: \
+ /home/i02veegc/segundo/ED/binary_tree2/binary_tree/test_btree_node.cpp \
+ /usr/local/include/stdc-predef.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/iostream \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/c++config.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/os_defines.h \
+ /usr/local/include/features.h /usr/local/include/sys/cdefs.h \
+ /usr/local/include/bits/wordsize.h /usr/local/include/bits/long-double.h \
+ /usr/local/include/gnu/stubs.h /usr/local/include/gnu/stubs-32.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ostream \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ios \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/iosfwd \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stringfwd.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/memoryfwd.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/postypes.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cwchar \
+ /usr/local/include/wchar.h /usr/local/include/bits/libc-header-start.h \
+ /usr/local/include/bits/floatn.h /usr/local/include/bits/floatn-common.h \
+ /usr/local/opt/gcc-5.3.0-32/lib/gcc/i686-pc-linux-gnu/5.3.0/include/stddef.h \
+ /usr/local/opt/gcc-5.3.0-32/lib/gcc/i686-pc-linux-gnu/5.3.0/include/stdarg.h \
+ /usr/local/include/bits/wchar.h /usr/local/include/bits/types/wint_t.h \
+ /usr/local/include/bits/types/mbstate_t.h \
+ /usr/local/include/bits/types/__mbstate_t.h \
+ /usr/local/include/bits/types/__FILE.h \
+ /usr/local/include/bits/types/FILE.h \
+ /usr/local/include/bits/types/locale_t.h \
+ /usr/local/include/bits/types/__locale_t.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/exception \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/atomic_lockfree_defines.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/exception_ptr.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/exception_defines.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/nested_exception.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/char_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_algobase.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/functexcept.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/cpp_type_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/type_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/numeric_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_pair.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/move.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/concept_check.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/type_traits \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_iterator_base_types.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_iterator_base_funcs.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/debug/debug.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_iterator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/ptr_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/predefined_ops.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cstdint \
+ /usr/local/opt/gcc-5.3.0-32/lib/gcc/i686-pc-linux-gnu/5.3.0/include/stdint.h \
+ /usr/local/include/stdint.h /usr/local/include/bits/types.h \
+ /usr/local/include/bits/timesize.h /usr/local/include/bits/typesizes.h \
+ /usr/local/include/bits/time64.h /usr/local/include/bits/stdint-intn.h \
+ /usr/local/include/bits/stdint-uintn.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/localefwd.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/c++locale.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/clocale \
+ /usr/local/include/locale.h /usr/local/include/bits/locale.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cctype \
+ /usr/local/include/ctype.h /usr/local/include/endian.h \
+ /usr/local/include/bits/endian.h /usr/local/include/bits/byteswap.h \
+ /usr/local/include/bits/uintn-identity.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/ios_base.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/atomicity.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/gthr.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/gthr-default.h \
+ /usr/local/include/pthread.h /usr/local/include/sched.h \
+ /usr/local/include/bits/types/time_t.h \
+ /usr/local/include/bits/types/struct_timespec.h \
+ /usr/local/include/bits/sched.h \
+ /usr/local/include/bits/types/struct_sched_param.h \
+ /usr/local/include/bits/cpu-set.h /usr/local/include/time.h \
+ /usr/local/include/bits/time.h /usr/local/include/bits/timex.h \
+ /usr/local/include/bits/types/struct_timeval.h \
+ /usr/local/include/bits/types/clock_t.h \
+ /usr/local/include/bits/types/struct_tm.h \
+ /usr/local/include/bits/types/clockid_t.h \
+ /usr/local/include/bits/types/timer_t.h \
+ /usr/local/include/bits/types/struct_itimerspec.h \
+ /usr/local/include/bits/pthreadtypes.h \
+ /usr/local/include/bits/thread-shared-types.h \
+ /usr/local/include/bits/pthreadtypes-arch.h \
+ /usr/local/include/bits/setjmp.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/atomic_word.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/locale_classes.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/string \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/allocator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/c++allocator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/new_allocator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/new \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/ostream_insert.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/cxxabi_forced.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_function.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/backward/binders.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/range_access.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/initializer_list \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/basic_string.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/alloc_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/alloc_traits.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/string_conversions.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cstdlib \
+ /usr/local/include/stdlib.h /usr/local/include/bits/waitflags.h \
+ /usr/local/include/bits/waitstatus.h /usr/local/include/sys/types.h \
+ /usr/local/include/sys/select.h /usr/local/include/bits/select.h \
+ /usr/local/include/bits/types/sigset_t.h \
+ /usr/local/include/bits/types/__sigset_t.h /usr/local/include/alloca.h \
+ /usr/local/include/bits/stdlib-float.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cstdio \
+ /usr/local/include/stdio.h /usr/local/include/bits/types/__fpos_t.h \
+ /usr/local/include/bits/types/__fpos64_t.h \
+ /usr/local/include/bits/types/struct_FILE.h \
+ /usr/local/include/bits/types/cookie_io_functions_t.h \
+ /usr/local/include/bits/stdio_lim.h \
+ /usr/local/include/bits/sys_errlist.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cerrno \
+ /usr/local/include/errno.h /usr/local/include/bits/errno.h \
+ /usr/local/include/linux/errno.h /usr/local/include/asm/errno.h \
+ /usr/local/include/asm-generic/errno.h \
+ /usr/local/include/asm-generic/errno-base.h \
+ /usr/local/include/bits/types/error_t.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/functional_hash.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/hash_bytes.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/basic_string.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/locale_classes.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/system_error \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/error_constants.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/stdexcept \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/streambuf \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/streambuf.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/basic_ios.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/locale_facets.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cwctype \
+ /usr/local/include/wctype.h /usr/local/include/bits/wctype-wchar.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/ctype_base.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/streambuf_iterator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/ctype_inline.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/locale_facets.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/basic_ios.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/ostream.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/istream \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/istream.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/fstream \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/codecvt.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/basic_file.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/i686-pc-linux-gnu/bits/c++io.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/fstream.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/sstream \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/sstream.tcc \
+ /home/i02veegc/segundo/ED/binary_tree2/binary_tree/btree.hpp \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/cassert \
+ /usr/local/include/assert.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/memory \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_construct.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_uninitialized.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_tempbuf.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_raw_storage_iter.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/typeinfo \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/concurrence.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/uses_allocator.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/functional \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/tuple \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/utility \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_relops.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/array \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/unique_ptr.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/shared_ptr.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/shared_ptr_base.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/allocated_ptr.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/ext/aligned_buffer.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/shared_ptr_atomic.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/atomic_base.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/backward/auto_ptr.h \
+ /home/i02veegc/segundo/ED/binary_tree2/binary_tree/btree_imp.hpp \
+ /home/i02veegc/segundo/ED/binary_tree2/binary_tree/btree_utils.hpp \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/queue \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/deque \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_deque.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/deque.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/vector \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_vector.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_bvector.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/vector.tcc \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_heap.h \
+ /usr/local/opt/gcc-5.3.0-32/include/c++/5.3.0/bits/stl_queue.h \
+ /home/i02veegc/segundo/ED/binary_tree2/binary_tree/btree_utils_imp.hpp
